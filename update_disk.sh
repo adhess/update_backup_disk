36b1f31c6db75f3d256ff738a547cf5a487a2f91
MYBK <med.yessine.b.k@gmail.com>
2018-08-23 10:30:58 +0100

    fix bugs

--- update_disk.sh
+++ update_disk.sh
@@ -1,61 +1,105 @@
 #!/bin/bash
-isOnly="false"
-isIn="false"
-mainPath=""
-fullPath=""
-isDiff="false"
-isMR="false"
-firstFile=""
-diff -r $1 $2 > modification_history
-for str in $(cat "modification_history") 
-do 
-	if [ "$str" = "Only" ]
-	then
-		isOnly="true"
-	elif [ "$str" = "in" ]
-	then
-		isIn="true"
-	elif [ "$isOnly" = "true" ] && [ "$isIn" = "true" ] && [ "$mainPath" = "" ]
-	then
-		
-		fullPath="${str::-1}"
-		mainPath=$(echo ${str::-1}| cut -d'/' -f 1)
-	elif [ "$isOnly" = "true" ] && [ "$isIn" = "true" ]
-	then
-#		echo "$path || $2  \n  "
-        if [ "$mainPath" = "$2" ]
-        then
-			secondPath=$( echo $fullPath|sed "s/$2//" )
-			echo "exist only: cp $fullPath/$str ====> $1$secondPath/$str"
-			cp -r "$fullPath/$str" "$1$secondPath/$str"
+
+diff -r "$1" "$2" > data
+exec 3< "$(pwd)/data"
+
+line=""
+i=0
+while read -u 3 -r line 
+do
+	
+	m1=$(echo $line | cut -d ' ' -f 1)
+	m2=$(echo $line | cut -d ' ' -f 2)
+
+	if [[ "$m1" = "Only" && "$m2" = "in" ]]; then
+		count=3
+
+		m1=$(echo $line | cut -d ' ' -f $count)
+		var2=$m1
+		while [[ ! "$m1" = "" ]]; do
+			((count++))
+			m1=$(echo $line | cut -d ' ' -f $count)
+			if [[ ! "$m1" = "" ]]; then
+				var2="$var2 $m1"
+			fi
+		done
+		var1=$(echo $var2 | cut -d ':' -f 2)
+		var1=$(echo $var1 | cut -d ' ' -f 2)
+
+		var2=$(echo $var2 | cut -d ':' -f 1)
+		var2="$var2/$var1"
+
+		count=2
+
+		m1=$(echo $var2 | cut -d '/' -f $count)
+		path=$m1
+		while [[ ! "$m1" = "" ]]; do
+			((count++))
+			m1=$(echo $var2 | cut -d '/' -f $count)
+			if [[ ! "$m1" = "" ]]; then
+				path="$path/$m1"
+			fi
+		done
+
+		nameFolder=$(echo $var2 | cut -d '/' -f 1)
+
+		if [[ "$nameFolder" = "$2" ]]; then
+			cp -r "$2/$path" "$1/$path" 
+			echo "copy o: $2/$path ===> $1"
 		fi
-		isOnly="false"
-		isIn="false"
-		mainPath=""
-		fullPath=""
-	fi
-	if [ "$str" = "diff" ]
-	then
-		isDiff="true"
-	elif [ "$str" = "-r" ]
-	then
-		isMR="true"
-	elif [ "$isDiff" = "true" ] && [ "$isMR" = "true" ] && [ "$firstFile" = "" ]
-	then
-		firstFile="$str"
-	elif [ "$isDiff" = "true" ] && [ "$isMR" = "true" ]
-	then
-		echo "same file: cp -----> $firstFile"
-        secondFile="$str"
-        rm "$firstFile"
-        cp "$secondFile" "$firstFile"
 
-		isDiff="false"
-		isMR="false"
-		firstFile=""
+	elif [[ "$m1" = "diff" && "$m2" = "-r" ]]; then
+		count=3
+
+		m1=$(echo $line | cut -d ' ' -f $count)
+		var2=$m1
+		while [[ ! "$m1" = "" ]]; do
+			((count++))
+			m1=$(echo $line | cut -d ' ' -f $count)
+			if [[ ! "$m1" = "" ]]; then
+				var2="$var2 $m1"
+			fi
+		done
+
+		file1=""
+		file2=""
+		if [[ $(echo $var2 | cut -d '"' -f 2) = $var2 ]]; then
+			file1="$(echo $var2 | cut -d ' ' -f 1)"
+			file2="$(echo $var2 | cut -d ' ' -f 2)"
+		else
+			file1=$(echo $var2 | cut -d '"' -f 2)
+			file2=$(echo $var2 | cut -d '"' -f 4)
+		fi
+
+		rm "$file1"
+		cp "$file2" "$file1"
+		echo "copy d: $file2 ==> $file1"
+
+	elif [[ "$m1" = "Binary" && "$m2" = "files" ]]; then
+		count=3
+
+		m1=$(echo $line | cut -d ' ' -f $count)
+		var2=$m1
+		while [[ ! "$m1" = "" ]]; do
+			((count++))
+			m1=$(echo $line | cut -d ' ' -f $count)
+			if [[ ! "$m1" = "" ]]; then
+				if [[ "$m1" = "and" ]]; then
+					file1="$var2"
+					var2=""
+				elif [[ "$m1" = "differ" ]]; then
+					file2="${var2:1}"
+					var2=""
+				else
+					var2="$var2 $m1"
+				fi
+			fi
+		done
+		rm "$file1"
+		cp "$file2" "$file1"
+		echo "copy b: $file2 ==> $file1"
 	fi
+	
 done
-echo "done we will check another time for unupdated changes"
-
-diff -r $1 $2 > final_result
-
+echo "--------------*******************************************---------------"
+diff -r "$1" "$2"
